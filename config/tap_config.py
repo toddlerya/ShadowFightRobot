37519b3092771c14ee3e78e21995a967e6e5911f
toddlerya <toddlerya@gmail.com>
2021-05-26 13:00:11 +0800

    增加手机任务

--- config/tap_config.py
+++ config/tap_config.py
@@ -26,3 +26,46 @@ button_points = {
 }
 
 
+CLICK_DUEL_BUTTON_EVENT = {
+    'desc': '打开决斗',
+    'value': button_points['open_fight']
+}
+
+
+CLICK_START_FIGHT_BUTTON_EVENT = {
+    'desc': '开始战斗',
+    'value': button_points['start_fight']
+}
+
+COMBO_EVENT = {
+    'desc': '开始攻击',
+    'value': button_points['fist']
+}
+
+END_FIGHT_CLICK_CONTINUE_EVENT = {
+    'desc': '结束战斗',
+    'value': button_points['end_fight_continue']
+}
+
+GOLD_DETAILS_CONTINUE_EVENT = {
+    'desc': '展示金币明细',
+    'value': button_points['end_fight_continue']
+}
+
+RECEIVE_AWARD_GOLD_CLICK_CONTINUE_EVENT = {
+    'desc': '领取战斗金币奖励',
+    'value': button_points['end_fight_continue']
+}
+
+
+annotation_event_map = {
+    # 'thunder_wait_open_game.png': OPEN_GAME_EVENT,
+    # 'wait_close_shop_ad.png': CLOSE_SHOP_AD_EVENT,
+    # 通过这里控制参加哪个活动==>game_home_page
+    'game_home_page.png': CLICK_DUEL_BUTTON_EVENT,
+    'wait_click_fight_button.png': CLICK_START_FIGHT_BUTTON_EVENT,
+    # 'wait_close_new_season_dialog.png': CLOSE_NEW_SEASON_DIALOG_EVENT,
+    'end_fight_click_continue.png': END_FIGHT_CLICK_CONTINUE_EVENT,
+    'gold_detail_click_continue.png': GOLD_DETAILS_CONTINUE_EVENT,
+    'receive_award_gold_click_continue.png': RECEIVE_AWARD_GOLD_CLICK_CONTINUE_EVENT
+}
--- robot.py
+++ robot.py
@@ -17,7 +17,7 @@ from PIL import Image
 from apscheduler.schedulers.background import BlockingScheduler
 
 from config import IMAGE_NAME, ANNOTATION_IMAGE_PATH
-from config.event_config_bs import *
+from config.tap_config import *
 from utils.trans import hex2dec
 from utils.operate import adb_shell, pull_screenshot
 from utils.image import read_image, ssim_score
@@ -25,81 +25,75 @@ from utils.image import read_image, ssim_score
 
 class EventManager:
     def __init__(self):
-        self.open_game_event_cmd_list = list()
-        self.close_shop_ad_event_cmd_list = list()
-        self.close_new_season_event_cmd_list = list()
-        self.click_duel_button_event_cmd_list = list()
-        self.start_fight_button_event_cmd_list = list()
-        self.combo_event_cmd_list = list()
-        self.end_fight_click_continue_event_cmd_list = list()
-        self.gold_details_click_continue_event_cmd_list = list()
-        self.receive_award_gold_click_continue_event_cmd_list = list()
+        self.open_game_event_cmd = ''
+        self.close_shop_ad_event_cmd = ''
+        self.close_new_season_event_cmd = ''
+        self.click_duel_button_event_cmd = ''
+        self.start_fight_button_event_cmd = ''
+        self.combo_event_cmd = ''
+        self.end_fight_click_continue_event_cmd = ''
+        self.gold_details_click_continue_event_cmd = ''
+        self.receive_award_gold_click_continue_event_cmd = ''
         self.ann_images = list()
         self.running_event = None
 
     @staticmethod
-    def __set_event(event):
-        for line in event.strip().split('\n'):
-            line = hex2dec(line)
-            __send_event_cmd = f"sendevent {line.replace(':', '')}"
-            yield __send_event_cmd
+    def __set_short_tap_event(event):
+        return f'input tap {event["x"]} {event["y"]}'
+
+    @staticmethod
+    def __set_long_tap_event(event):
+        return f'input touchscreen swipe {event["x"]} {event["y"]} {event["x"]} {event["y"]} 500'
 
     def loader(self):
         """
         加载并构建sendevent命令
         """
-        self.open_game_event_cmd_list = [ele for ele in self.__set_event(OPEN_GAME_EVENT['value'])]
-        self.close_shop_ad_event_cmd_list = [ele for ele in self.__set_event(CLOSE_SHOP_AD_EVENT['value'])]
-        self.close_new_season_event_cmd_list = [ele for ele in self.__set_event(CLOSE_NEW_SEASON_DIALOG_EVENT['value'])]
-        self.click_duel_button_event_cmd_list = [ele for ele in self.__set_event(CLICK_DUEL_BUTTON_EVENT['value'])]
-        self.start_fight_button_event_cmd_list = [ele for ele in self.__set_event(CLICK_START_FIGHT_BUTTON_EVENT['value'])]
-        self.combo_event_cmd_list = [ele for ele in self.__set_event(COMBO_EVENT['value'])]
-        self.end_fight_click_continue_event_cmd_list = [ele for ele in self.__set_event(END_FIGHT_CLICK_CONTINUE_EVENT['value'])]
-        self.gold_details_click_continue_event_cmd_list = [ele for ele in self.__set_event(GOLD_DETAILS_CONTINUE_EVENT['value'])]
-        self.receive_award_gold_click_continue_event_cmd_list = [ele for ele in self.__set_event(RECEIVE_AWARD_GOLD_CLICK_CONTINUE_EVENT['value'])]
+        # self.open_game_event_cmd = self.__set_event(OPEN_GAME_EVENT['value'])]
+        # self.close_shop_ad_event_cmd = self.__set_event(CLOSE_SHOP_AD_EVENT['value'])]
+        # self.close_new_season_event_cmd = self.__set_event(CLOSE_NEW_SEASON_DIALOG_EVENT['value'])]
+        self.click_duel_button_event_cmd = self.__set_short_tap_event(CLICK_DUEL_BUTTON_EVENT['value'])
+        self.start_fight_button_event_cmd = self.__set_short_tap_event(CLICK_START_FIGHT_BUTTON_EVENT['value'])
+        self.combo_event_cmd = self.__set_long_tap_event(COMBO_EVENT['value'])
+        self.end_fight_click_continue_event_cmd = self.__set_short_tap_event(END_FIGHT_CLICK_CONTINUE_EVENT['value'])
+        self.gold_details_click_continue_event_cmd = self.__set_short_tap_event(GOLD_DETAILS_CONTINUE_EVENT['value'])
+        self.receive_award_gold_click_continue_event_cmd = self.__set_short_tap_event(RECEIVE_AWARD_GOLD_CLICK_CONTINUE_EVENT['value'])
         self.ann_images = glob.glob(f'{str(pathlib.Path(ANNOTATION_IMAGE_PATH).absolute())}/*.png')
 
     def sender(self, event_name):
         """
         执行sendevent命令
         """
-        logger.info(f'sendevent {event_name}')
+        logger.info(f'input tap {event_name}')
         if event_name == '打开游戏':
-            for cmd in self.open_game_event_cmd_list:
-                adb_shell(cmd)
+            adb_shell(self.open_game_event_cmd)
             time.sleep(60)
         if event_name == '关闭商店活动广告':
-            for cmd in self.close_shop_ad_event_cmd_list:
-                adb_shell(cmd)
+            adb_shell(self.close_shop_ad_event_cmd)
             time.sleep(5)
         if event_name == '打开决斗':
-            for cmd in self.click_duel_button_event_cmd_list:
-                adb_shell(cmd)
-            time.sleep(30)
+            adb_shell(self.click_duel_button_event_cmd)
+            time.sleep(15)
         if event_name == '开始战斗':
-            for cmd in self.start_fight_button_event_cmd_list:
-                adb_shell(cmd)
+            adb_shell(self.start_fight_button_event_cmd)
             time.sleep(3)
             self.fight()
         if event_name == '结束战斗':
-            for cmd in self.end_fight_click_continue_event_cmd_list:
-                adb_shell(cmd)
+            adb_shell(self.end_fight_click_continue_event_cmd)
         if event_name == '展示金币明细':
-            for cmd in self.gold_details_click_continue_event_cmd_list:
-                adb_shell(cmd)
+            adb_shell(self.gold_details_click_continue_event_cmd)
         if event_name == '领取战斗金币奖励':
-            pass
+            adb_shell(self.receive_award_gold_click_continue_event_cmd)
 
-    @logger.catch
+    @logger.catch(reraise=True)
     def fight(self):
         logger.info('开始攻击')
-        for i in range(10):
+        for i in range(60):
             logger.info(f'第{i + 1}次攻击')
-            for cmd in self.combo_event_cmd_list:
-                adb_shell(cmd)
+            adb_shell(self.combo_event_cmd)
             time.sleep(0.5)
 
-    @logger.catch
+    @logger.catch(reraise=True)
     def chose_event(self):
         """
         根据当前屏幕图像，匹配图片事件标记，选择执行的事件
@@ -119,7 +113,7 @@ class EventManager:
         logger.info(f'推荐事件: {recommend_event["desc"]}, 匹配成功图片: {similarity_img}, 相似度: {max_score}')
         return recommend_event
 
-    @logger.catch
+    @logger.catch(reraise=True)
     def run_event(self):
         """
         执行事件
@@ -131,7 +125,7 @@ class EventManager:
         self.sender(__event_desc)
 
 
-@logger.catch
+@logger.catch(reraise=True)
 def check_screenshot():
     """
     检查获取截图的方式
