248639f0a7137eb5268f6812bc47c15f66d26986
toddlerya <toddlerya@gmail.com>
2021-05-26 22:11:34 +0800

    更新打开游戏截图事件

--- robot.py
+++ robot.py
@@ -73,13 +73,14 @@ class EventManager:
             time.sleep(5)
         if event_name == '打开决斗':
             adb_shell(self.click_duel_button_event_cmd)
-            time.sleep(15)
+            time.sleep(20)
         if event_name == '开始战斗':
             adb_shell(self.start_fight_button_event_cmd)
-            time.sleep(3)
+            time.sleep(5)
             self.fight()
         if event_name == '结束战斗':
             adb_shell(self.end_fight_click_continue_event_cmd)
+            time.sleep(5)
         if event_name == '展示金币明细':
             adb_shell(self.gold_details_click_continue_event_cmd)
         if event_name == '领取战斗金币奖励':
