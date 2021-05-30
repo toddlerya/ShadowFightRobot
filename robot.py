434b013344d8863ba59f2ead468d78da2ca7eebc
toddlerya <toddlerya@gmail.com>
2021-05-30 20:52:37 +0800

    增加网络连接失败事件

--- robot.py
+++ robot.py
@@ -33,6 +33,7 @@ class EventManager:
         self.gold_details_click_continue_event_cmd = ''
         self.receive_award_gold_click_continue_event_cmd = ''
         self.lose_fight_tip_click_continue_event_cmd = ''
+        self.connect_error_click_continue_event_cmd = ''
         self.ann_images = list()
         self.running_event = None
 
@@ -60,6 +61,8 @@ class EventManager:
             RECEIVE_AWARD_GOLD_CLICK_CONTINUE_EVENT['value'])
         self.lose_fight_tip_click_continue_event_cmd = self.__set_short_tap_event(
             LOSE_FIGHT_TIP_CLICK_CONTINUE_EVENT['value'])
+        self.connect_error_click_continue_event_cmd = self.__set_short_tap_event(
+            CONNECT_ERROR_CLICK_CONTINUE_EVENT['value'])
         self.ann_images = glob.glob(f'{str(pathlib.Path(ANNOTATION_IMAGE_PATH).absolute())}/*.png')
 
     def sender(self, event_name):
@@ -91,6 +94,8 @@ class EventManager:
             self.fight()
         if event_name == '输了比赛':
             adb_shell(self.lose_fight_tip_click_continue_event_cmd)
+        if event_name == '网络连接失败':
+            adb_shell(self.connect_error_click_continue_event_cmd)
 
     @logger.catch(reraise=True)
     def fight(self):
